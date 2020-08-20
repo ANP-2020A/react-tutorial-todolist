9e1ef3ecbdaae825032073b1bc82a980f89fb569
chalosalvador <chalosalvador@gmail.com>
2020-08-20 16:28:31 -0500

    useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 import UserList from './UserList';
 import TodoList from './TodoList';
 
-const App = () => (
-  <>
-    <UserList />
-    <TodoList />
-  </>
-);
+const App = () => {
+
+  const [ viewTasks, setViewTasks ] = useState( true );
+
+  return (
+    <>
+      <UserList />
+
+      <button onClick={ () => setViewTasks( ( prevViewTasks ) => !prevViewTasks ) }>
+        { viewTasks
+          ? 'Ocultar'
+          : 'Ver' } lista de tareas
+      </button>
+      {
+        viewTasks && <TodoList />
+      }
+    </>
+  );
+};
 
 export default App;
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,57 @@
 /**
  * Created by chalosalvador on 8/2/20
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/todo-list.css';
+import Spinner from './Spinner';
 
 const TodoList = () => {
 
-  const [ todos, setTodos ] = React.useState( [] );
-  const [ completed, setCompleted ] = React.useState( [] );
+  const [ todos, setTodos ] = useState( [] );
+  const [ completed, setCompleted ] = useState( [] );
+  const [ darkMode, setDarkMode ] = useState( false );
+  const [ userInfo, setUserInfo ] = useState( null );
+  const [ windowWith, setWindowWith ] = useState( window.innerWidth );
 
+  useEffect( () => {
+    const getData = async() => {
+      const data = await fetch( 'https://jsonplaceholder.typicode.com/users/1' );
+      const dataJson = await data.json();
+      setUserInfo( dataJson );
+    };
+    getData();
+  }, [] );
+
+  useEffect( () => {
+    console.log( 'efecto', todos.length );
+    if( todos.length > 0 ) {
+      document.title = `${ todos.length } tareas pendientes`;
+    } else {
+      document.title = `No tienes tareas pendientes`;
+    }
+  }, [ todos ] );
+
+  useEffect( () => {
+    console.log( 'El nuevo estado es: ', darkMode
+      ? 'DARK MODE'
+      : 'LIGHT  MODE' );
+  }, [ darkMode ] );
+
+  useEffect( () => {
+    console.log( 'El COMPONENTE SE MONTO' );
+
+    window.addEventListener( 'resize', handleResize );
+
+    return () => {
+      console.log( 'EL COMPONENTE SE DESMONTO' );
+      window.removeEventListener( 'resize', handleResize );
+    };
+  } );
+
+  const handleResize = () => {
+    console.log( window.innerWidth );
+    setWindowWith( window.innerWidth );
+  };
 
   const handleAddTask = () => {
     const task = document.querySelector( '#task' ).value;
@@ -30,9 +74,41 @@ const TodoList = () => {
     handleDeleteTask( index );
   };
 
+  const handleDarkMode = () => {
+    setDarkMode( ( prevDarkMode ) => !prevDarkMode );
+  };
 
   return (
-    <div>
+    <div className={ darkMode
+      ? 'dark-mode'
+      : '' }>
+
+
+      <h1>El ancho de la ventana es: { windowWith }</h1>
+
+
+      <div>
+        <h1>Información del usuario</h1>
+        {
+          userInfo
+            ?
+            <ul>
+              <li>{ userInfo.name }</li>
+              <li>{ userInfo.email }</li>
+              <li>{ userInfo.website }</li>
+              <li>{ userInfo.phone }</li>
+            </ul>
+            : <Spinner />
+        }
+      </div>
+
+
+      <button onClick={ handleDarkMode }>
+        Cambiar a modo { darkMode
+        ? 'claro'
+        : 'oscuro' }
+      </button>
+
       <div>
         <label htmlFor='task'>Tarea</label>
         <input type='text' id='task' />
